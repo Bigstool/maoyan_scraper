c6df08cf3ba709d53d579ee8d5883537ca2bfdbb
Bigstool <20697772+Bigstool@users.noreply.github.com>
2021-10-28 16:29:38 +0800

    scraper: get movie index

--- maoyan_scraper/scraper.py
+++ maoyan_scraper/scraper.py
@@ -5,34 +5,47 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup as bs
-
-# sys.path.append('./geckodriver.exe')  # Uncomment if using Firefox for Windows, comment otherwise
-# sys.path.append('./geckodriver')  # Uncomment if using Firefox for Linux, comment otherwise
+from typing import List
 
 root_url = 'https://maoyan.com'
 movie_index_path = 'board/4'
 
 
 def captcha_handler(driver):
-    while '猫眼验证中心' in driver.page_source:
-        driver.implicitly_wait(5)
+    while '猫眼验证中心' in driver.page_source:  # wait for captcha to be manually solved
+        driver.implicitly_wait(5)  # check the webpage once every 5 seconds
 
 
 def get_movie_index(offset: int = 0):
-    # driver = webdriver.Firefox(executable_path='./geckodriver')
-    # driver.get(parse.urljoin(root_url, f'{movie_index_path}?offset={offset}'))
-    # captcha_handler(driver)
-    # WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, 'dd'))
-    # source = driver.page_source
-    # source = bs(source, 'html.parser').prettify()
-    #
-    # with open(file='./dummy.html', mode='w', encoding='utf-8') as f:
-    #     f.write(source)
+    driver = webdriver.Firefox(executable_path='./geckodriver')  # Uncomment if using Firefox for Linux x64
+    # driver = webdriver.Firefox(executable_path='./geckodriver.exe')  # Uncomment if using Firefox for Windows
 
-    # Experiment with dummy start
-    with open('./dummy.html', encoding='utf-8') as f:
-        source = f.read()
-    source = bs(source, 'html.parser').prettify()
+    movie_list = []
+
+    while offset < 100:
+        driver.get(parse.urljoin(root_url, f'{movie_index_path}?offset={offset}'))
+        captcha_handler(driver)
+        WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, 'dd'))  # wait for element to load
+        source = driver.page_source
+        source = bs(source, 'html.parser')
+
+        movie_entries = source.find_all('dd')
+        for entry in movie_entries:
+            rank: int = int(entry.i.get_text().strip())  # strip() removes whitespace
+            link: str = entry.a.get('href')
+            title: str = entry.a.get('title')
+            stars: List[str] = entry.find('p', class_='star').get_text().strip()[3:]
+            time: str = entry.find('p', class_='releasetime').get_text().strip()[5:]
+            rating_raw: str = entry.find('i', class_='integer').get_text().strip() + \
+                              entry.find('i', class_='fraction').get_text().strip()
+            rating: float = float(rating_raw)
+            movie = {'rank': rank, 'title': title, 'link': link, 'stars': stars, 'time': time, 'rating': rating}
+            movie_list.append(movie)
+
+        offset += 10
+
+    for movie in movie_list:
+        print(movie)
 
     # elements = driver.find_elements(By.TAG_NAME, 'dd')
     # for e in elements:
